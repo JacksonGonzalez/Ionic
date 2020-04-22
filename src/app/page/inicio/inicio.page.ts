eae2412363d7acd5b56e4048625f1db4376dc4bb
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-04-22 14:48:26 -0500

    ion-list

--- src/app/page/inicio/inicio.page.ts
+++ src/app/page/inicio/inicio.page.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  componentes: componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'construct-outline',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+    ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+
+interface componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
