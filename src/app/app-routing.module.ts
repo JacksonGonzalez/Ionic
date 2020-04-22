75c668ff520a8c9dc9d11b2d064bfa7b966d6c7e
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-04-21 20:10:08 -0500

    navegacion simple

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,14 @@ const routes: Routes = [
     path: 'inicio',
     loadChildren: () => import('./page/inicio/inicio.module').then( m => m.InicioPageModule)
   },
+  {
+    path: 'alert',
+    loadChildren: () => import('./page/alert/alert.module').then( m => m.AlertPageModule)
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () => import('./page/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
+  },
 ];
 
 @NgModule({
