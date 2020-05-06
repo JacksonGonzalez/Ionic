7b771b6e51ea8391c1a719478e3323d03df38946
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-05-06 15:38:08 -0500

    ion-skeleton-text para dar un esqueleto a la data antes de que se muestre

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { componente } from '../interfaces/interfaces';
 
+import { delay } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -23,6 +25,9 @@ export class DataService {
   }
 
   getHeroes(){
-    return this.http.get('/assets/data/superheroes.json');
+    return this.http.get('/assets/data/superheroes.json')
+      .pipe(
+        delay(2000)
+      );
   }
 }
