9dfa7bc7d68721a1aa59da36b6a722b46b11b2dc
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-05-06 16:50:47 -0500

    ion-toast para dar notificaciones visuales

--- src/app/page/list/list.page.ts
+++ src/app/page/list/list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -12,26 +13,75 @@ export class ListPage implements OnInit {
 
   usuarios: Observable<any>;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private toastCtrl : ToastController) { }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
     
   }
 
-  favorite(user){
-    console.log('favorite', user);
+  async favorite(user){
+    // console.log('favorite', user);
+    const toast = await this.toastCtrl.create({
+      message: 'Eres el Favorito:  ' + user.username,
+      duration: 2000
+    });
+    toast.present();
     
   }
 
-  share(user){
-    console.log('share', user);
+   async share(user){
+    // console.log('share', user);
+    const toast = await this.toastCtrl.create({
+      header: 'Compartir',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
     
   }
 
-  unread(user){
+  async unread(user){
     console.log('unread', user);
     
+    const toast = await this.toastCtrl.create({
+      header: 'Eliminar',
+      message: 'Desea eliminar a '+ user.name,
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Eliminar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
   }
 
 
